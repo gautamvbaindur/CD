dadb83c961a536ce42f703cb87f2c6001387d2d9
gautamvbaindur <g.baindur@prowareness.nl>
2014-08-08 10:31:41 +0530

    First GIT checkin

---
+++ MathsLibrary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class MathsLibrary
+    {
+        public int Add(int p1, int p2)
+        {
+            return p1 + p2;
+        }
+
+        public int Subtract(int p1, int p2)
+        {
+            return p1 - p2;
+        }
+
+        public int Multiply(int p1, int p2)
+        {
+            return p1 * p2;
+        }
+    }
+}
